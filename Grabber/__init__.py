308da351e1d90a92a1be4f5bb7ffb0c0bb251f5e
VG-TEAM <152384469+VG-TEAM@users.noreply.github.com>
2024-02-07 14:59:38 +0530

    Update __init__.py

--- Grabber/__init__.py
+++ Grabber/__init__.py
@@ -19,7 +19,7 @@ LOGGER = logging.getLogger(__name__)
 OWNER_ID = 5932230962
 sudo_users = ["5932230962", "5932230962"]
 GROUP_ID = -1001924285977
-TOKEN = "6512496443:AAEoBRxK2cP4l3qK8TKbDx54F8Mw9398eTs"
+TOKEN = "6512496443:AAFKX49iM1N6Go0wYDACLOvae33TNK_hqss"
 mongo_url = "mongodb+srv://husbando:husbando@cluster0.pipkivx.mongodb.net/?retryWrites=true&w=majority"
 PHOTO_URL = ["https://telegra.ph/file/72ea883532b722f405059.jpg", "https://telegra.ph/file/72ea883532b722f405059.jpg"]
 SUPPORT_CHAT = "TEAMS_VG"
